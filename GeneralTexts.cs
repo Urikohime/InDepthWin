4e578f389b871ce45ee60857e5913ace4a11a5cf
Hime <Xodakat@LIVE.DE>
2019-09-19 12:12:05 +0200

    -First update startmenu Faction texts

--- GeneralTexts.cs
+++ GeneralTexts.cs
@@ -8,11 +8,35 @@ namespace InDepthWin
 {
     class GeneralTexts
     {
-        public String UEDF = "UEDF TEST";
-        public String CPMC = "CPMC TEST";
-        public String CONM = "CONM TEST";
-        public String TSIB = "TSIB TEST";
-        public String NLFD = "NLFD TEST";
-        public String AOBO = "AOBO TEST";
+        public String UEDF = "United Earth Defence Force\nFacing the RUOP threat, the Earth united to fight them off.\n" +
+            "Now roaming and exploring the universe to find new places to live and find new rescources.";
+
+        public String CPMC = "Cemita Private Military Contractors\n" +
+            "Earthlings that did not agree with the intention to search for new planets.\n" +
+            "With the intention to stop the Earthlings from populating they rather picked up a more military life.\n" +
+            "From birth on they are trained to withstand harsh conditions.";
+
+        public String CONM = "Colonies Of the Nexus Mercenaries\n" +
+            "Those are the Earthling that wanted to conquer planets.\n" +
+            "Supporting the UEDF on different occasions.";
+
+        public String TSIB = "Tjr Sim Id Bsf\n" +
+            "There is no recorded history of their own, but others suspect they have been living inside a giant Planet.\n" +
+            "Only rarely sending out one ship every decade to explore.\n" +
+            "Their technology is unknown";
+
+        public String NLFD = "New Lunar Federation Division\n" +
+            "Trying to populate the universe they came into a conflict with the UEDF and becoming allies with the CPMC.\n" +
+            "They are only a small 'Division' from the New Lunar Federation, this Division is responsible for this universe";
+
+        public String AOBO = "Alliance Of the Black Orbit\n" +
+            "Text will come for them later because i'm fucking out of ideas hHAHAHAhahaHhAHah";
+
+        public String[] UEDFSTORY = { "" };
+        public String[] CPMCSTORY = { "" };
+        public String[] CONMSTORY = { "" };
+        public String[] TSIBSTORY = { "" };
+        public String[] NLFDSTORY = { "" };
+        public String[] AOBOSTORY = { "" };
     }
 }
